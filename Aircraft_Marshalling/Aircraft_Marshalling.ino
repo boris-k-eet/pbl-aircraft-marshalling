7c61bb3d6fd251a6045e7c06e79552dfd5e100ba
boris-k-eet <boris.kosenko@dawsoncollege.qc.ca>
2025-05-06 10:40:07 -0400

    Add to the code skeleton

--- Aircraft_Marshalling/Aircraft_Marshalling.ino
+++ Aircraft_Marshalling/Aircraft_Marshalling.ino
@@ -1,7 +1,6 @@
 #include <NewPing.h>
 #include <LiquidCrystal.h>  
 
-const int buttonPin = 3;
 const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
 
 #define TRIGGER_PIN  12  
@@ -16,19 +15,29 @@ const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  // Create an LCD object
 
-void flash_LED(){
-  pinMode(LED_PIN)
-}
+bool flashing;
 
+void flash_LED(){
+
+}
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int distance = sonar.ping()
-  
+  lcd.clear();
+  int distance = sonar.ping();
+
+  if (distance < STOP_DISTANCE){
+    // LED is ON and the LCD displays prints 'STOP!'
+  } else if (distance < FLASHING_DISTANCE){
+    // LED is flashing and the LCD displays the distance
+  } else {
+    // LED is off and the LCD displays the distance
+  }
+
 
 }
